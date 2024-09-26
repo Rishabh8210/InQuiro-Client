17cd72cefdc08ca82206320743385c881c335fdd
Rishabh Pandey <rishabhpandey8092@gmail.com>
2024-09-27 01:06:08 +0530

    Commit : Fixing Navbar and added sub-menu navbar

--- src/components/HeaderComponent.tsx
+++ src/components/HeaderComponent.tsx
@@ -1,10 +1,13 @@
 import { Navbar } from "./Navbar"
-import { MobileNavbar } from "./MobileNavbar"
+import { MobileNavbar, SubNavbarMenu } from "./MobileNavbar"
 export function HeaderComponent(){
     return (
-        <div className="h-16 w-full px-7 md:px-32 bg-zinc-900">
-            <Navbar />
-            <MobileNavbar />
-        </div>
+        <>
+            <div className="h-16 w-full px-5 md:px-32 bg-zinc-900 border-b-2">
+                <Navbar />
+                <MobileNavbar />
+            </div>
+            <SubNavbarMenu />
+        </>
     )
 }
--- src/components/MobileNavbar.tsx
+++ src/components/MobileNavbar.tsx
@@ -15,9 +15,15 @@ export function MobileNavbar() {
 
     return (
         <>
-            <div className="md:hidden h-full w-full flex justify-center items-center">
-                <div className=" h-full w-full flex items-center justify-between">
+            <div className="md:hidden h-full w-full flex justify-between items-center">
+                <div className=" h-full w-fit flex items-center ">
                     <img className="dark:invert" src='/hamburger-menu.png' alt="#" onClick={() => handleClick()}/>
+                </div>
+                <div className="h-fit w-fit flex justify-center items-center">
+                    <h1 className="text-3xl font-san font-bold text-red-600">Quora</h1>
+                </div>
+                <div className="h-fit w-fit">
+                    <Link href='/post'><img className="invert" src="/addButton.png"/></Link>
                 </div>
             </div>
             {isNavOpen && <SidebarMenu setTrigger = {handleClick}/>}
@@ -29,14 +35,36 @@ export function SidebarMenu(props: HandleProps) {
     return (
         <div className="h-screen w-full absolute top-0 left-0 p-10 flex flex-col  gap-10 bg-black">
             <div className="w-full h-fit py-5 flex justify-end">
-                <img className="invert h-10 pointer" src="/closeButton.png" onClick={() => props.setTrigger()}/>
+                <img className="dark:invert h-9 pointer" src="/closeButton.png" onClick={() => props.setTrigger()}/>
             </div>
-            <ul className="h-fit flex flex-col justify-center items-center border-2 rounded-md w-full ">
-                <li className="p-5 h-fit"><Link className= 'text-lg text-red-600 font-semibold' href='/'>Home</Link></li>
-                <li className="p-5 h-fit"><Link className= 'text-lg font-semibold' href='/following'>Following</Link></li>
-                <li className="p-5 h-fit"><Link className= 'text-lg font-semibold' href='/answer'>Answer</Link></li>
-                <li className="p-5 h-fit"><Link className= 'text-lg font-semibold' href='/notifications'>Notification</Link></li>
+            <ul className="h-fit flex flex-col justify-center items-center border-2 border-white border-opacity-40 rounded-md w-full ">
+                <li className="p-5 h-fit"><Link className= 'text-2xl text-red-600 font-semibold' href='/'>Home</Link></li>
+                <li className="p-5 h-fit"><Link className= 'text-2xl font-semibold' href='/following'>Following</Link></li>
+                <li className="p-5 h-fit"><Link className= 'text-2xl font-semibold' href='/answer'>Answer</Link></li>
+                <li className="p-5 h-fit"><Link className= 'text-2xl font-semibold' href='/notifications'>Notification</Link></li>
             </ul>
+        </div>
+    )
+}
+
+export function SubNavbarMenu() {
+    return (
+        <div className="h-14 w-full flex justify-center items-center bg-zinc-900 border-b-2 border-opacity-40 ">
+            <Link href='/' className="h-full px-6 w-fit flex-shrink-0 flex justify-center items-center border-r-2">
+                <img className="h-7 " src="/homeButton.png"/>
+            </Link>
+            <Link href='/' className="h-full px-6 flex-shrink-0 w-fit flex justify-center items-center border-r-2">
+                <img className="h-7 dark:invert" src="/followingButton.png"/>
+            </Link>
+            <Link href='/' className="h-full px-6 flex-shrink-0 w-fit flex justify-center items-center border-r-2">
+                <img className="h-7 dark:invert" src="/notesButton.png"/>
+            </Link>
+            <Link href='/' className="h-full px-6 flex-shrink-0 w-fit flex justify-center items-center border-r-2">
+                <img className="h-7 dark:invert" src="/notificationsButton.png"/>
+            </Link>
+            <Link href='/' className="h-full px-6 flex-shrink-0 w-fit flex justify-center items-center border-r-2">
+                <img className="h-7 dark:invert" src="/userButton.png"/>
+            </Link>
         </div>
     )
 }
